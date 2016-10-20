135b6295454c8e66ead3ceb5caab6d9cd28a8c1b
renxia <lzwy0820@qq.com>
2016-10-20 20:32:40 +0800

    更新 cli, useage

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
  * 启动 node-server
  */
 
-exports.name = 'start';
-exports.usage = '<template>';
+exports.name = 'start <options>';
 exports.desc = 'mznode start command.';
 exports.options = {
     '--server': 'start nginx and node-server.',
@@ -16,6 +15,9 @@ exports.run = function (argv, cli) {
     // 如果输入为 fis3 foo -h
     // 或者 fis3 foo --help
     // 则输出帮助信息。
+if (argv.h || argv.help) {
+        return cli.help(exports.name, exports.options);
+    }
 
     if (argv.server) {
         // 启动 nginx 和 node-server
@@ -26,8 +28,10 @@ exports.run = function (argv, cli) {
     } else if (argv.node) {
         // 启动 node-server
         require('./lib/start')(2);
-    } else {
+    } else if(argv.build) {
         // 启动项目构建
         require('./lib/build')();
+    } else {
+        cli.help(exports.name, exports.options);
     }
 }
