8ccd59f650e34a59ec58c48921f9c48310d4f958
李志文 <lizhiwen@meizu.com>
2016-10-20 15:13:56 +0800

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * 启动 node-server
+ */
+
+exports.name = 'start';
+exports.usage = '<template>';
+exports.desc = 'mznode start command.';
+exports.options = {
+    '--server': 'start nginx and node-server.',
+    '--nginx': 'start nginx.',
+    '--node': 'start node-server.',
+    '--build': 'start build.'
+};
+
+exports.run = function (argv, cli) {
+    // 如果输入为 fis3 foo -h
+    // 或者 fis3 foo --help
+    // 则输出帮助信息。
+
+    if (argv.server) {
+        // 启动 nginx 和 node-server
+        require('./lib/start')();
+    } else if (argv.nginx) {
+        // 启动 nginx
+        require('./lib/start')(1);
+    } else if (argv.node) {
+        // 启动 node-server
+        require('./lib/start')(2);
+    } else {
+        // 启动项目构建
+        require('./lib/build')();
+    }
+}
