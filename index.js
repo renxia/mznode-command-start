dd53bc510441ba51fdbdb0c02ebfdb64da803ffc
renxia <lzwy0820@qq.com>
2017-10-17 17:18:12 +0800

    逻辑更新优化; version 1.0.7

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ exports.run = function (argv, cli) {
     // 如果输入为 fis3 foo -h
     // 或者 fis3 foo --help
     // 则输出帮助信息。
-if (argv.h || argv.help) {
+    if (argv.h || argv.help) {
         return cli.help(exports.name, exports.options);
     }
 
     if (argv.server) {
         // 启动 nginx 和 node-server
-        require('./lib/start')();
+        require('./lib/start')(0, argv.m);
     } else if (argv.nginx) {
         // 启动 nginx
-        require('./lib/start')(1);
+        require('./lib/start')(1, argv.m);
     } else if (argv.node) {
         // 启动 node-server
-        require('./lib/start')(2);
+        require('./lib/start')(2, argv.m);
     } else if(argv.build) {
         // 启动项目构建
         require('./lib/build')();
